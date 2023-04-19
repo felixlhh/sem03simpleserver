09c57960cf713d57f1daf5315c05a6b509c52cbb
felixlhh <felixlh@uia.no>
2023-04-19 15:58:27 +0000

    kryptert

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/felixlhh/sem03simpleserver
+
+go 1.18
+
+require github.com/felixlhh/is105sem03 v0.0.0-20230418143729-fc6fab841861 // indirect
--- tcp_server.go
+++ tcp_server.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"sync"
+	"github.com/felixlhh/is105sem03/mycrypt"
+
 )
 
 func main() {
@@ -36,6 +38,21 @@ func main() {
 						return // fra for løkke
 					}
 					switch msg := string(buf[:n]); msg {
+  				        case "ping":
+						_, err = c.Write([]byte("pong"))
+					default:
+						_, err = c.Write(buf[:n])
+					}
+					if err != nil {
+						if err != io.EOF {
+							log.Println(err)
+						}
+						return // fra for løkke
+					}
+				dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+					msg := string(dekryptertMelding)
+					switch msg {
   				        case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
