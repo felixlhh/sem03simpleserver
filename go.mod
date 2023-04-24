a76b8298625f584c24507442b9e0c9d32c093bb9
felixlhh <felixlh@uia.no>
2023-04-24 08:15:25 +0000

    funker

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/felixlhh/sem03simpleserver
 
 go 1.18
 
-require github.com/felixlhh/is105sem03 v0.0.0-20230418143729-fc6fab841861 // indirect
+require github.com/felixlhh/is105sem03 v0.0.0-20230424075720-f7e91c4ca5d9
+
+require (
+	github.com/felixlhh/funtemps v0.0.0-20230221180431-bed20e19062a // indirect
+	github.com/felixlhh/kjevikConv v0.0.0-20230421063353-80fd1e0853f5 // indirect
+)
--- tcp_server.go
+++ tcp_server.go
@@ -1,17 +1,53 @@
 package main
 
 import (
+	//"fmt"
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 	"sync"
-	"github.com/felixlhh/is105sem03/mycrypt"
 
+	"github.com/felixlhh/is105sem03/mycrypt"
 )
+
+func celsiusToFahrenheit(temp float64) float64 {
+
+	return (temp * 9 / 5) + 32
+
+}
+
+func konverterSetning(input string) string {
+
+	parts := strings.Split(input, ";")
+
+	if len(parts) != 4 {
+		log.Println("Please input valid data")
+	}
+
+	tempvalue, err := strconv.ParseFloat(parts[3], 64)
+	if err != nil || parts[3] == "" {
+		log.Println("Error parsing temperature:", err)
+	}
+
+	fahrenheit := celsiusToFahrenheit(tempvalue)
+	fahrenheitString := strconv.FormatFloat(fahrenheit, 'f', 1, 64)
+
+	parts[3] = fahrenheitString
+
+	konvertertSetning := strings.Join(parts, ";")
+
+	return konvertertSetning
+
+}
 
 func main() {
 
+	log.Println("test")
+
 	var wg sync.WaitGroup
+
 	server, err := net.Listen("tcp", "172.17.0.3:1234")
 	if err != nil {
 		log.Fatal(err)
@@ -37,12 +73,25 @@ func main() {
 						}
 						return // fra for løkke
 					}
-
+					log.Println("Buffer: ", string(buf[:n]))
 					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 					log.Println("Dekrypter melding: ", string(dekryptertMelding))
-					switch msg := string(dekryptertMelding); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
+					msg := string(dekryptertMelding)
+					log.Println("msg :", msg)
+					splitmsg := strings.Split(msg, ";")
+					log.Println("Splitmsg: ", splitmsg)
+
+					switch splitmsg[0] {
+					case "ping":
+						kryptertMelding := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+						log.Println("Kryptert melding: ", string(kryptertMelding))
+						_, err = c.Write([]byte(string(kryptertMelding)))
+					case "Kjevik":
+						convertTemp := konverterSetning(msg)
+						log.Println(convertTemp)
+						kryptertMelding := mycrypt.Krypter([]rune(convertTemp), mycrypt.ALF_SEM03, 4)
+						log.Println("Kryptert melding: ", string(kryptertMelding))
+						_, err = c.Write([]byte(string(kryptertMelding)))
 					default:
 						_, err = c.Write(buf[:n])
 					}
@@ -58,3 +107,4 @@ func main() {
 	}()
 	wg.Wait()
 }
+
